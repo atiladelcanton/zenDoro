2400a9cbc6974d12076232650868f16ebfc2bb96
Atila Rampazo <atilarampazo@gmail.com>
2021-02-27 10:33:34 -0300

    feat: Ajuste

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
   const minutes = Math.floor(time / 60);
@@ -33,7 +33,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   }
   function resetCountDown() {
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     clearTimeout(countDownTimeout);
     setHasFinished(false);
   }
